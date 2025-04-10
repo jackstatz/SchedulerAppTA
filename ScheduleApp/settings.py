b9f0f5fcb6e3b6d6828b05b5df2bbe2ee96c2c72
devty <barjrwork@gmail.com>
2025-04-10 11:35:19 -0500

    Added login.html // Added bootstrap //

--- ScheduleApp/settings.py
+++ ScheduleApp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ScheduleAppData.apps.ScheduleAppConfig',
+    'authenticate',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
