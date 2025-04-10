f85a72f7d38e58e9fccf052bbac46ad9f6ad8167
devty <barjrwork@gmail.com>
2025-04-10 09:41:10 -0500

    Added authentication app

--- authenticate/apps.py
+++ authenticate/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InstructorConfig(AppConfig):
+class AuthenticateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'instructor'
+    name = 'authenticate'
