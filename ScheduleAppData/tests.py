2395d7313f9b6a5dca28e83e6a456f03a1a31b4c
PinkertonCal <pinkertonfam@gmail.com>
2025-04-22 21:59:21 -0500

    Login Tests

--- ScheduleAppData/tests.py
+++ ScheduleAppData/tests.py
@@ -1,9 +1,65 @@
-from django.test import TestCase
-
+from django.test import override_settings
+from django.test import TestCase, Client
+from django.urls import reverse
+from ScheduleAppData.models import Courses as CoursesModel # Rename to avoid conflict with Courses view
+from ScheduleAppData.models import User
+from django.urls import resolve
+from django.urls import Resolver404
 # Create your tests here.
 
 from django.test import TestCase
 from .models import Courses  # Assuming the class is in course_module.py
+
+class AdminLoginTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.login_url = '/'
+
+        # test admin user
+        if User:
+            self.admin_email = 'supervisor@uwm.edu'
+            self.admin_password = 'password'
+            self.admin_user = User.objects.create(
+                Email=self.admin_email,
+                Password=self.admin_password,
+                Role='Supervisor'
+            )
+    def test_valid_admin_login(self):
+
+        # Perform the login POST
+        initial_response = self.client.post(self.login_url, {'email': self.admin_email,'password': self.admin_password,})
+
+        #Assert the redirect is correct
+        self.assertEqual(initial_response.status_code, 302)
+        self.assertEqual(initial_response['Location'], '/adminDashboard')
+        final_response = self.client.get('/adminDashboard/', follow=True)
+        self.assertEqual(final_response.status_code, 200)
+
+    def test_invalid_password_admin_login(self):
+        response = self.client.post(self.login_url, {'email': self.admin_email,'password': 'wrongpassword',})
+        self.assertEqual(response.status_code, 200) # Should return, showing the login page
+        self.assertTemplateUsed(response, 'LoginPage.html')
+
+    def test_no_password_admin_login(self):
+        response = self.client.post(self.login_url, { 'email': self.admin_email, 'password': '',})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'LoginPage.html')
+
+    def test_empty(self):
+        response = self.client.post('/', {"username": "", "password": ""})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'LoginPage.html')
+
+    def test_wrong_username(self):
+        response = self.client.post('/', {"username": "wrong", "password": "password"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'LoginPage.html')
+
+    def test_wrong_password(self):
+        response = self.client.post('/', {"username": self.admin_email, "password": "Super wrong!!!"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'LoginPage.html')
 
 class TestCourses(TestCase):
 
